03409184a1d7e1cf3b5a836f66cd50919b7a7f28
Duke-He <1602871858@qq.com>
2020-08-21 00:09:22 +0800

    init

---
+++ src/components/button/interface.tsx
@@ -0,0 +1,67 @@
+import { AnchorHTMLAttributes, ButtonHTMLAttributes, ReactNode } from "react";
+
+export type AppearancesTypes = keyof typeof APPEARANCES;
+
+type btnType =
+  | "primary"
+  | "primaryOutline"
+  | "secondary"
+  | "secondaryOutline"
+  | "tertiary"
+  | "outline"
+  | "inversePrimary"
+  | "inverseSecondary"
+  | "inverseOutline";
+
+type AppearancesObj = {
+  [key in btnType]: btnType;
+};
+
+export const APPEARANCES: AppearancesObj = {
+  primary: "primary",
+  primaryOutline: "primaryOutline",
+  secondary: "secondary",
+  secondaryOutline: "secondaryOutline",
+  tertiary: "tertiary",
+  outline: "outline",
+  inversePrimary: "inversePrimary",
+  inverseSecondary: "inverseSecondary",
+  inverseOutline: "inverseOutline",
+};
+
+export type SizesTypes = keyof typeof SIZES;
+type sizeType = "small" | "medium";
+type sizeObj = {
+  [key in sizeType]: sizeType;
+};
+
+export const SIZES: sizeObj = {
+  small: "small",
+  medium: "medium",
+};
+
+export interface CustormButtonProps {
+  /** 是否禁用 */
+  disabled?: boolean;
+  /** 是否加载中 */
+  isLoading?: boolean;
+  /** 是否是a标签 */
+  isLink?: boolean;
+  /** 是否替换加载中文本 */
+  loadingText?: ReactNode;
+  /** 按钮大小 */
+  size?: SizesTypes;
+  /** 按钮类型 */
+  appearance?: AppearancesTypes;
+  /** 无效点击 */
+  isUnclickable?: boolean;
+}
+
+export type ButtonProps = CustormButtonProps &
+  AnchorHTMLAttributes<HTMLAnchorElement> &
+  ButtonHTMLAttributes<HTMLButtonElement>;
+
+export const btnPadding = {
+  small: "8px 16px",
+  medium: "13px 20px",
+};
